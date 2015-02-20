2345234db28ce41e260454d2b0cd94a5b49c8f67
Max Niebergall <max@maxniebergall.com>
2015-02-19 19:55:50 -0500

    Added name asking

--- src/Snake.java
+++ src/Snake.java
@@ -6,7 +6,9 @@ import java.io.InputStreamReader;
 public class Snake {
 
 	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Whats your Name?");
 		String name = br.readLine();
 		System.out.println("Hellow World");
 		System.out.println(name);
