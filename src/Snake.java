c99550fe13de0e70525c8e46871d1ac86006523f
Max Niebergall <max@maxniebergall.com>
2015-02-19 19:53:46 -0500

    Added Reading

--- src/Snake.java
+++ src/Snake.java
@@ -1,8 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Snake {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String name = br.readLine();
 		System.out.println("Hellow World");
+		System.out.println(name);
 
 	}
 
