6e30daf7f0f66cc0d320d37279a15d7bafbaa0dd
MaxNiebergall <max@maxniebergall.com>
2015-02-25 12:07:18 -0500

    Added pause (doesn't work)

--- src/Snake.java
+++ src/Snake.java
@@ -12,16 +12,14 @@ import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 
-/*Things to add:
- * Menu:(Includes)
- * 		About page
- * 		Difficulty (speed)
- * Highscores (Text file)
- *  
+/*
+ * Things to add: Menu:(Includes) About page Difficulty (speed) Highscores (Text
+ * file)
  */
 
 /**
@@ -30,137 +28,140 @@ import javax.swing.KeyStroke;
  * 
  * @link https://github.com/MaxNiebergall/Snake.git
  */
-public class Snake {
-
-	public static void main(String[] args) {
+public class Snake{
+	
+	public static void main(String[] args){
 		new Snake();
 	}
-
+	
+	boolean paused;
+	
 	// GUI Object Declarations
 	JFrame frame = new JFrame();
 	JPanel pane = new JPanel(new GridLayout(20, 20));
 	Point food = new Point();
-	JButton comList[][] = new JButton[20][20];// X, Y
-
+	JLabel comList[][] = new JLabel[20][20];// X, Y
+	
 	ArrayList<SnakeObject> snake = new ArrayList<SnakeObject>();
-
-	Snake() {
-
-		for (int x = 0; x < comList.length; x++) {
-			for (int y = 0; y < comList[x].length; y++) {
-				comList[x][y] = new JButton();
+	
+	Snake(){
+		
+		for(int x = 0; x < comList.length; x++){
+			for(int y = 0; y < comList[x].length; y++){
+				comList[x][y] = new JLabel();
 				comList[x][y].setMinimumSize(new Dimension(10, 10));
 				comList[x][y].setMaximumSize(new Dimension(10, 10));
+				comList[x][y].setOpaque(true);
 			}
 		}
 		System.out.println("comList set up");
 		
-		//test placeFood
-		for(int i=0; i<1000; i++){
+		// test placeFood
+		for(int i = 0; i < 1000; i++){
 			placeFood();
 		}
 		
-
 		reset(true);
 		System.out.println("reset(true)");
-
+		
 		// HERE ARE THE KEY BINDINGS
-		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-				KeyStroke.getKeyStroke(KeyEvent.VK_W, 0), "up");
-		pane.getActionMap().put("up", new AbstractAction() {
+		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0), "up");
+		pane.getActionMap().put("up", new AbstractAction(){
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e){
 				System.out.println("Up");
 				snake.get(0).setDirection(Direction.NORTH);
 			}
 		});
-		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-				KeyStroke.getKeyStroke(KeyEvent.VK_A, 0), "left");
-		pane.getActionMap().put("left", new AbstractAction() {
+		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0), "left");
+		pane.getActionMap().put("left", new AbstractAction(){
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e){
 				System.out.println("Left");
 				snake.get(0).setDirection(Direction.WEST);
 			}
 		});
-		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-				KeyStroke.getKeyStroke(KeyEvent.VK_D, 0), "right");
-		pane.getActionMap().put("right", new AbstractAction() {
+		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0), "right");
+		pane.getActionMap().put("right", new AbstractAction(){
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e){
 				System.out.println("Right");
 				snake.get(0).setDirection(Direction.EAST);
 			}
 		});
-		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-				KeyStroke.getKeyStroke(KeyEvent.VK_S, 0), "down");
-		pane.getActionMap().put("down", new AbstractAction() {
+		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0), "down");
+		pane.getActionMap().put("down", new AbstractAction(){
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e){
 				System.out.println("Down");
 				snake.get(0).setDirection(Direction.SOUTH);
 			}
 		});
-		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-				KeyStroke.getKeyStroke("UP"), "UP");
-		pane.getActionMap().put("UP", new AbstractAction() {
+		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("UP"), "UP");
+		pane.getActionMap().put("UP", new AbstractAction(){
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e){
 				System.out.println("Up-arrow");
 				snake.get(0).setDirection(Direction.NORTH);
 			}
 		});
-		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-				KeyStroke.getKeyStroke("LEFT"), "LEFT");
-		pane.getActionMap().put("LEFT", new AbstractAction() {
+		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("LEFT"), "LEFT");
+		pane.getActionMap().put("LEFT", new AbstractAction(){
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e){
 				System.out.println("Left-arrow");
 				snake.get(0).setDirection(Direction.WEST);
 			}
 		});
-		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-				KeyStroke.getKeyStroke("RIGHT"), "RIGHT");
-		pane.getActionMap().put("RIGHT", new AbstractAction() {
+		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("RIGHT"), "RIGHT");
+		pane.getActionMap().put("RIGHT", new AbstractAction(){
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e){
 				System.out.println("Right-arrow");
 				snake.get(0).setDirection(Direction.EAST);
 			}
 		});
-		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-				KeyStroke.getKeyStroke("DOWN"), "DOWN");
-		pane.getActionMap().put("DOWN", new AbstractAction() {
+		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("DOWN"), "DOWN");
+		pane.getActionMap().put("DOWN", new AbstractAction(){
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e){
 				System.out.println("Down-arrow");
 				snake.get(0).setDirection(Direction.SOUTH);
 			}
 		});
+		pane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_P, 0), "pause");
+		pane.getActionMap().put("pause", new AbstractAction(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				System.out.println("Pause/Play");
+				paused=!paused;
+				infiniteRefresh();
+				
+			}
+		});
 		// END OF KEY BINDINGS
-
+		
 		// add it all up
-		for (int x = 0; x < comList.length; x++) {
-			for (int y = 0; y < comList[x].length; y++) {
+		for(int x = 0; x < comList.length; x++){
+			for(int y = 0; y < comList[x].length; y++){
 				pane.add(comList[y][x]);
 			}
 		}
 		frame.add(pane);
-
+		
 		// frame window closer
-		frame.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
+		frame.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
 				System.exit(0);
 			}
 		});
-
+		
 		// frame settings
 		frame.setTitle("Max Nieberall's Snake Game v0.1.01 Beta"/*
-																 * TODO PUT
-																 * VERSION STUFF
-																 * HERE
+																 * TODO PUT VERSION
+																 * STUFF HERE
 																 */);
-
+		
 		frame.setSize(750, 700);
 		frame.setResizable(true);
 		frame.setLocationRelativeTo(null);
@@ -169,116 +170,83 @@ public class Snake {
 		frame.repaint();
 		// End GUI set up
 		// ---------------------------------------------------------------------
-
+		
 		infiniteRefresh();
 	}
-
-	private void extendSnake() {
+	
+	private void extendSnake(){
 		snake.add(new SnakeObject(snake.get(snake.size() - 1)));
 	}
-
-	private void updateComList() {
+	
+	private void updateComList(){
 		System.out.println("updateComList");
-		for (int x = 0; x < comList.length; x++) {
-			for (int y = 0; y < comList[x].length; y++) {
+		for(int x = 0; x < comList.length; x++){
+			for(int y = 0; y < comList[x].length; y++){
 				comList[x][y].setBackground(Color.WHITE);
 			}
 		}
-		for (SnakeObject snk : snake) {
-			comList[snk.getPoint().x][snk.getPoint().y]
-					.setBackground(Color.BLACK);
+		for(SnakeObject snk: snake){
+			System.out.println("Update comList snk: " + snk.getPoint());
+			if(snk.getPoint().getX() >= 0 && snk.getPoint().getY() >= 0 && snk.getPoint().getX() < comList.length && snk.getPoint().getY() < comList[0].length){
+				comList[snk.getPoint().x][snk.getPoint().y].setBackground(Color.BLACK);
+			}
+			
 		}
-		comList[(int) food.getX()][(int) food.getY()]
-				.setBackground(Color.GREEN);
+		comList[(int) food.getX()][(int) food.getY()].setBackground(Color.GREEN);
 	}
-
-	private boolean refresh() {
+	
+	private boolean refresh(){
 		System.out.println("Refresh()");
+		if(paused) return false;
 		// Update Snake
 		Direction dir0 = snake.get(0).getDirection(), dir1;
-		if ((snake.get(0).getPoint().x
-				+ ((snake.get(0).getDirection() == Direction.EAST || snake.get(
-						0).getDirection() == Direction.WEST) ? snake.get(0)
-						.direction() : 0) < 0
-				|| snake.get(0).getPoint().x
-						+ ((snake.get(0).getDirection() == Direction.EAST || snake
-								.get(0).getDirection() == Direction.WEST) ? snake
-								.get(0).direction() : 0) >= comList.length
-				|| snake.get(0).getPoint().y
-						+ ((snake.get(0).getDirection() == Direction.NORTH || snake
-								.get(0).getDirection() == Direction.SOUTH) ? snake
-								.get(0).direction() : 0) < 0 || snake.get(0)
-				.getPoint().y
-				+ ((snake.get(0).getDirection() == Direction.NORTH || snake
-						.get(0).getDirection() == Direction.SOUTH) ? snake.get(
-						0).direction() : 0) >= comList[0].length)) {
-
+		if((snake.get(0).getPoint().x + ((snake.get(0).getDirection() == Direction.EAST || snake.get(0).getDirection() == Direction.WEST) ? snake.get(0).direction() : 0) < 0 || snake.get(0).getPoint().x + ((snake.get(0).getDirection() == Direction.EAST || snake.get(0).getDirection() == Direction.WEST) ? snake.get(0).direction() : 0) >= comList.length || snake.get(0).getPoint().y + ((snake.get(0).getDirection() == Direction.NORTH || snake.get(0).getDirection() == Direction.SOUTH) ? snake.get(0).direction() : 0) < 0 || snake.get(0).getPoint().y + ((snake.get(0).getDirection() == Direction.NORTH || snake.get(0).getDirection() == Direction.SOUTH) ? snake.get(0).direction() : 0) >= comList[0].length)){
 			youLose();
 			return false;
-
 		}
-		System.out.println("snake.get(0).point: "
-				+ snake.get(0).getPoint().toString());
-		if (comList[snake.get(0).getPoint().x
-				+ ((snake.get(0).getDirection() == Direction.EAST || snake.get(
-						0).getDirection() == Direction.WEST) ? snake.get(0)
-						.direction() : 0)][snake.get(0).getPoint().y
-				+ ((snake.get(0).getDirection() == Direction.NORTH || snake
-						.get(0).getDirection() == Direction.SOUTH) ? snake.get(
-						0).direction() : 0)].getBackground() == Color.GREEN) {
+		System.out.println("snake.get(0).point: " + snake.get(0).getPoint().toString());
+		if(comList[snake.get(0).getPoint().x + ((snake.get(0).getDirection() == Direction.EAST || snake.get(0).getDirection() == Direction.WEST) ? snake.get(0).direction() : 0)][snake.get(0).getPoint().y + ((snake.get(0).getDirection() == Direction.NORTH || snake.get(0).getDirection() == Direction.SOUTH) ? snake.get(0).direction() : 0)].getBackground() == Color.GREEN){
 			extendSnake();
 			placeFood();
 		}
-		for (SnakeObject snk : snake) {
-
+		for(SnakeObject snk: snake){
+			
 			System.out.println("Snk forward");
 			snk.forward();
-
-			System.out.println(" SnkPoint: " + snk.getPoint() + " SnakePoint: "
-					+ snake.get(0).getPoint());// TODO Delete this line when the
-												// problem is resolved
-			System.out
-					.println(snk != snake.get(0)
-							&& (snake.get(0).getPoint().equals(snk.getPoint()) || (snk
-									.getPoint().x < 0
-									|| snk.getPoint().x >= comList.length
-									|| snk.getPoint().y < 0 || snk.getPoint().y >= comList[0].length)));
-			if ((snk != snake.get(0) && snake.get(0).getPoint()
-					.equals(snk.getPoint()))
-					|| (snk.getPoint().x < 0
-							|| snk.getPoint().x >= comList.length
-							|| snk.getPoint().y < 0 || snk.getPoint().y >= comList[0].length)) {
-
+			
+			System.out.println(" SnkPoint: " + snk.getPoint() + " SnakePoint: " + snake.get(0).getPoint());
+			System.out.println(snk != snake.get(0) && (snake.get(0).getPoint().equals(snk.getPoint()) || (snk.getPoint().x < 0 || snk.getPoint().x >= comList.length || snk.getPoint().y < 0 || snk.getPoint().y >= comList[0].length)));
+			if((snk != snake.get(0) && snake.get(0).getPoint().equals(snk.getPoint())) || (snk.getPoint().x < 0 || snk.getPoint().x >= comList.length || snk.getPoint().y < 0 || snk.getPoint().y >= comList[0].length)){
+				
 				youLose();
 				return false;
-
+				
 			}
-
+			
 			dir1 = snk.getDirection();
 			snk.setDirection(dir0);
 			dir0 = dir1;
-
+			
 		}
-
+		
 		// Update GUI
 		updateComList();
 		frame.validate();
 		frame.repaint();
-
+		
 		return true;
 	}
-
-	private void youLose() {
-		int option = JOptionPane.showConfirmDialog(pane,
-				"You Lose :-(\nwould you like to play again?");
-		if (option == JOptionPane.YES_OPTION) {
+	
+	private void youLose(){
+		int option = JOptionPane.showConfirmDialog(pane, "You Lose :-(\nwould you like to play again?");
+		if(option == JOptionPane.YES_OPTION){
 			reset(false);
-
-		} else {
+			
+		}else{
 			System.exit(0);
 		}
 	}
-
+	
 	/**
 	 * reset() resets the snake if first is false, it re-initiates the
 	 * inifiniteRefresh loop
@@ -286,81 +254,77 @@ public class Snake {
 	 * @param Boolean
 	 *            first
 	 */
-	private void reset(Boolean first) {
+	private void reset(Boolean first){
 		// Restart the board state
 		snake.clear();
-		snake.add(new SnakeObject(new Point(comList.length / 2,
-				comList[0].length / 2), Direction.NORTH));
+		snake.add(new SnakeObject(new Point(comList.length / 2, comList[0].length / 2), Direction.NORTH));
 		extendSnake();
 		extendSnake();
 		System.out.println("extendSnake2");
 		placeFood();
 		System.out.println("placeFood()");
-
-		if (!first)
-			infiniteRefresh();
-
+		
+		if(!first) infiniteRefresh();
+		
 		// End restarting the board state
 	}
-
-	private void infiniteRefresh() {
-
+	
+	private void infiniteRefresh(){
+		
 		// Infinite loop to update board state
 		// ----------------------------------------------------------------------
 		boolean refresh;
-		do {
-			System.out.println("While True");
+		do{
+			System.out.println("While refresh");
 			long startTime = System.currentTimeMillis();
 			refresh = refresh();
 			long endTime = System.currentTimeMillis(); // 166 milliseconds is
 														// 1/60 seconds, or 60Hz
-			try {
-				Thread.sleep((166 - (endTime - startTime) > 0) ? 166 - (endTime - startTime)
-						: 0);
-			} catch (InterruptedException e1) {
+			try{
+				Thread.sleep((166 - (endTime - startTime) > 0) ? 166 - (endTime - startTime) : 0);
+			}catch(InterruptedException e1){
 				e1.printStackTrace();
 			}
-		} while (refresh);
+		}while(refresh);
 		// End infinite loop to update board state
 		// ----------------------------------------------------------------------
 	}
-
+	
 	/**
 	 * placeFood() changes the background of an element of comList to green to
 	 * represent a food the element selected is a random integer between 0 and
 	 * (comList.length * comList[0].length)
 	 */
-	private void placeFood() {
+	private void placeFood(){
 		// Find random position
-		int x = (int) (Math.random() * (comList.length - 1)), y = (int) (Math
-				.random() * (comList[0].length - 1));
-
+		int x = (int) (Math.random() * (comList.length - 1)), y = (int) (Math.random() * (comList[0].length - 1));
+		
 		// find next empty position
 		boolean empty = true;
-		do {
-			do {
-				for (SnakeObject snk : snake) {
-					if (snk.getPoint().distance(x, y) == 0) {
-						System.out.println((char)27 + "[31mThis text would show up red" + (char)27 + "[0m");
-						System.out.println((char)27 + "distance"+snk.getPoint().distance(x,y)+ (char)27 + "[0m");
+		do{
+			do{
+				for(SnakeObject snk: snake){
+					if(snk.getPoint().distance(x, y) == 0){
+						System.out.println((char) 27 + "[31mThis text would show up red" + (char) 27 + "[0m");
+						System.out.println((char) 27 + "distance" + snk.getPoint().distance(x, y) + (char) 27 + "[0m");
 						empty = false;
 					}
 				}
-				System.out.println("empty y: |" + y + "| x: |" + x
-						+ "|");
+				System.out.println("empty y: |" + y + "| x: |" + x + "|");
 				++y;
-			} while (y < 20 && !empty);
-			y=1;
+			}while(y < 20 && !empty);
+			if(y >= 20) y = 1;
 			++x;
-		} while (x < 20 && !empty);
-		x-=1;y-=1;
-		System.out.println("x "+x+" y "+y);
+		}while(x < 20 && !empty);
+		x -= 1;
+		y -= 1;
+		System.out.println("x " + x + " y " + y);
 		food.setLocation(x, y);
 		System.out.println("Food Placed: x:" + x + " y: " + y);
 		updateComList();
-
+		
 	}
-
+	
 	// for (x = 0; count > 0; ++x) {
 	// System.out.println("Random count: |" + count + "|");
 	// if (x == 19) {
